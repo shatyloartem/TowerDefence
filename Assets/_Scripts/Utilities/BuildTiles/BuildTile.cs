2feecef4aa2be5f4a8ccf50a277784482ca49269
shatyloartem <59529985+shatyloartem@users.noreply.github.com>
2023-08-15 13:04:19 +0300

    Making tile upgrade panel

--- Assets/_Scripts/Utilities/BuildTiles/BuildTile.cs
+++ Assets/_Scripts/Utilities/BuildTiles/BuildTile.cs
@@ -1,12 +1,24 @@
+using TD.UI;
 using UnityEngine;
+using Zenject;
 
 namespace TD.Tiles
 {
     public class BuildTile : MonoBehaviour, IInteractable
     {
+        public Transform Transform { get; private set; }
+
+        [Inject]
+        private ITileUpgradePanelManager _upgradePanelManager;
+
+        private void Awake()
+        {
+            Transform = transform;
+        }
+
         public void Interact()
         {
-
+            _upgradePanelManager.SpawnPanelOnTile(this);
         }
     }
 }
--- Assets/_Scripts/Utilities/BuildTiles/TileUpgradePanelManager.cs
+++ Assets/_Scripts/Utilities/BuildTiles/TileUpgradePanelManager.cs
@@ -1,14 +1,31 @@
-﻿using UnityEngine;
+﻿using TD.Tiles;
+using UnityEngine;
 
 namespace TD.UI
 {
     public class TileUpgradePanelManager : MonoBehaviour, ITileUpgradePanelManager
     {
+        [SerializeField]
+        private GameObject _upgradePanel;
 
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        public void SpawnPanelOnTile(BuildTile tile)
+        {
+            Vector3 screenSpacePosition = _camera.WorldToScreenPoint(tile.Transform.position);
+            
+            _upgradePanel.transform.position = screenSpacePosition;
+            _upgradePanel.SetActive(true);
+        }
     }
 
     public interface ITileUpgradePanelManager
     {
-
+        public void SpawnPanelOnTile(BuildTile tile);
     }
 }
